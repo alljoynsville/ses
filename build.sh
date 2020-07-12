8cc6b2f11aac47840fd30e6e9f54be7120d4bdce
Eliav Gnessin <eliav.gnessin@devicetone.com>
2020-07-12 14:47:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 docker build config_3.50_sdk_15.0.0 -t josschne/ses:3.50_sdk_15.0.0
 docker build config_3.50_sdk_15.2.0 -t josschne/ses:3.50_sdk_15.2.0
+docker build config_3.50_sdk_16.0.0 -t josschne/ses:3.50_sdk_16.0.0
 
 #Test the new image with a smoke test
 GET_VERSION=$(docker run -it josschne/ses:3.50_sdk_15.0.0 /ses/bin/emBuild | grep Release)
 GET_VERSION=$(docker run -it josschne/ses:3.50_sdk_15.2.0 /ses/bin/emBuild | grep Release)
+GET_VERSION=$(docker run -it josschne/ses:3.50_sdk_16.0.0 /ses/bin/emBuild | grep Release)
